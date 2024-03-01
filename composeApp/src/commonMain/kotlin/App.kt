5300d6dad22990f488dab78ff9bee4048a49bba4
Miguel Lasa <miguel.lasa@decemberlabs.com>
2024-03-01 16:07:03 +0100

    GHAT2.

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -27,7 +27,7 @@ fun App() {
         val greeting = remember { Greeting().greet() }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             QButton(
-                text = "QButton Auto 1",
+                text = "QButton Auto 2",
                 onClick = { showContent = !showContent },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color(0xff6200EE) //colorResource(id = R.color.purple_500)
